14cae301c9851c427ad30c2132cf58f9534267c7
JX531 <91365967+JX531@users.noreply.github.com>
2024-08-09 21:14:59 +1000

    Update Snake.py

--- Snake1/Snake.py
+++ Snake1/Snake.py
@@ -1,11 +1,11 @@
 class Snake:
     def __init__(self,board_size):
-        self.spawn = [[board_size//2,board_size//2]]
-        self.body = self.spawn
-        self.direction = 0 #up
-        self.grow = False
+        self.spawn = [[board_size//2,board_size//2]] #Spawn location with 1 segment in middle of board
+        self.body = self.spawn 
+        self.direction = 0 #starts with up // 0 = up, 1 = down, 2 = left, 3 = right
+        self.grow = False #grow ticks to True whenever it eats apple to add new segment, then back to false
     
-    def reset(self):
+    def reset(self): #reset snake
         self.body = self.spawn
         self.direction = 0
         self.grow = False
@@ -13,7 +13,7 @@ class Snake:
     def move(self):
         new_head = self.body[0].copy()
 
-        x = 0
+        x = 0 #using x y coordinates to improve readibility
         y = 1
 
         if (self.direction == 0): #up
@@ -28,21 +28,21 @@ class Snake:
         elif (self.direction == 3): #right
             new_head[x] += 1
 
-        if (self.grow):
-            self.body = [new_head] + self.body
-            self.grow = False
+        if (self.grow): #if set to grow
+            self.body = [new_head] + self.body #appends entire old body to new head
+            self.grow = False #turn off grow after growing
 
         else:
-            self.body = [new_head] + self.body[:-1]
+            self.body = [new_head] + self.body[:-1] #appends everything except very last node to new head
     
-    def changeDirection(self, new_direction):
+    def changeDirection(self, new_direction): # to stop snake from reversing into itself
         opposite_directions = {
             0: 1,
             1: 0,
             2: 3,
             3: 2
         }
-        if (new_direction in opposite_directions and self.direction != opposite_directions[new_direction]):
-            self.direction = new_direction
+        if (new_direction in opposite_directions and self.direction != opposite_directions[new_direction]): #not reversing
+            self.direction = new_direction #change direction
 
     
